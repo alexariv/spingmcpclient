6de80b24db16caab1862c23c58f54f48e1287a55
alexariv <alexarivera921@gmail.com>
2025-08-11 10:52:05 -0400

    Initial commit

---
+++ src/main/java/olog/springMCPclient/SpringMcPclientApplication.java
@@ -0,0 +1,13 @@
+package olog.springMCPclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMcPclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMcPclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/olog/springMCPclient/SpringMcPclientApplicationTests.java
@@ -0,0 +1,13 @@
+package olog.springMCPclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMcPclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
